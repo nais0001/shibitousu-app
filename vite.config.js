ba9ecc6b0c68f0356c1d7c067147552b674a4e57
nais0001 <nais0001@gmail.com>
2026-05-25 16:27:09 +0900

    Fix: Vite自身の置換バグに対する完全な先回り定義

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   define: {
+    // 1. 先ほどエラーになったHMRの変数を安全な文字列にする
+    '__HMR_CONFIG_NAME__': '"shibi-hmr"',
+    '__BASE__': '"/"',
+    
+    // 2. env.mjs 内で漏れている __DEFINES__ を安全なオブジェクトにする
     '__DEFINES__': '{}',
     'global.__DEFINES__': '{}',
     'process.env': {}
